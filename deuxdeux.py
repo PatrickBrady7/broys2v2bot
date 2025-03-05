44a608b347d42a3de94a65d3a9eb8deb2f7e7a5a
PatrickBrady7 <97626904+PatrickBrady7@users.noreply.github.com>
2025-03-05 15:01:52 -0500

    Add files via upload
    Included new stats in spreadsheet to reduce need to traverse spreadsheet and computation leading to reduced runtime when commands are entered
    Standardized formulas for all stats to be rounded to 2 decimals and be based off of players' total rounds in events rather than avg of stats at end of games. This is to prevent skewing of results as rounds are not equal from game to game which could cause abnormal stats(think HS%).   
    Added new columns in spreadsheet for MVP, EVP and Placements. These are used to give event info with a new function - eventStats()
    eventStats() - allows users to pull details of an event such as mvp, evps, winner, etc.
    
    Once again some of this is framework and still needs to be tested/implement fully.
    Added comments at top to signify future plans/idea

--- deuxdeux.py
+++ deuxdeux.py
@@ -1,20 +1,31 @@
 #import pandas as pd
-stats = open("2v2stats.xsl")
+
+#Use sheets in excel to differentiate between tournaments?
+#Have a master sheet that pulls data from all other sheets as like a lifetime stats?
+#give user option with stats to look at lifetime vs a specific event?
+#player object, pull params from other columns in excel after the name??
+
+#***Included some of the stats in spreadsheet to reduce need to traverse spreadsheet and computation leading to reduced runtime
+#***Standardized formulas for all stats to be rounded to 2 decimals and be based off of players' total rounds in events rather than avg of stats at end of games. This is to prevent skewing of results as rounds are not equal from game to game which could cause abnormal stats(think HS%).   
+#***Add columns in spreadsheet to signify mvp and winners??
+#***maybe allow users to pull some kind of generic stats for an event... mvp, stat leaders, winner, etc.
+
+#pd.read_excel("2v2stats.xlsx")
+stats = open("2v2stats.xslx")
 
 def HSpercentage(self, player):
     #separate by rows
     for entry in stats: 
         if entry == player.name:  #player input will determine row
-            K = columB
-            HS = columC
+            HSP = columnN
 
-    print("HS%: " + K/HS)
+    print("HS%: " + HSP)
 
 def ADR(self, player):
     #separate by rows
     for entry in stats: 
         if entry == player.name:  #player input will determine row
-            ADR = columG  
+            ADR = columnG  
     
     print("ADR: " + ADR)
 
@@ -22,52 +33,47 @@ def KPR(self, player):
     #separate by rows
     for entry in stats: 
         if entry == player.name:  #player input will determine row
-            K = columB
-            R = columH
+            KPR = columnI
     
-    print("KPR: " + K/R)   
+    print("KPR: " + KPR)   
   
 def DPR(self, player):
     #separate by rows
     for entry in stats: 
         if entry == player.name:  #player input will determine row
-            D = columD
-            R = columH
+            DPR = columnJ
     
-    print("DPR: " + D/R)   
+    print("DPR: " + DPR)   
 
 def EFR(self, player):
     #separate by rows
     for entry in stats: 
         if entry == player.name:  #player input will determine row
-            EF = columF
-            R = columH
+            EFR = columnL
     
-    print("EF per Round: " + EF/R)  
+    print("EF per Round: " + EFR)  
 
 def UDR(self, player):
     #separate by rows
     for entry in stats: 
         if entry == player.name:  #player input will determine row
-            UD = columE
-            R = columH
+            UDR = columnK
     
-    print("UD per Round: " + UD/R)  
+    print("UD per Round: " + UDR)  
 
 def KD(self, player):
     #separate by rows
     for entry in stats: 
         if entry == player.name:  #player input will determine row
-            K = columB
-            D = columD
+            KD = columnM
     
-    print("KD: " + K/D)
+    print("KD: " + KD)
 
 def Rounds(self, player):
     #separate by rows
     for entry in stats: 
         if entry == player.name:  #player input will determine row
-            R = columH
+            R = columnH
     print(R)     
 
 def playerStats(self, player):
@@ -77,13 +83,39 @@ def playerStats(self, player):
     ADR(self, player)
     print("\t")
     HSpercentage(self, player)
-    print("\n")
+    print()
     KPR(self, player)
     print("\t")
     DPR(self, player)
-    print("\n")
+    print()
     UDR(self, player)
     print("\t")
     EFR(self, player)
-    print("\n")
+    print()
     #print("'!advStats *player*' for advanced stats")
+
+def eventStats(self, tournamnet):
+    winners = []
+    EVPs = [] #List for EVPs
+    MVP = ""
+    print("Summary fo stats for " + tournamnet + ":\n") #Grab from sheet name?
+    #Iterate to find mvp/evp/placements
+    for entry in stats:
+        name = ""
+        name = columnA 
+        for column in entry:
+            if column == "MVP": 
+                MVP = name
+            elif column == "EVP":
+                EVPs.append(name)
+            elif column == "1st":
+                winners.append(name)
+
+                
+    print("Winners: " + winners + "\n") #column P
+    print("MVP: " + MVP + "\n") #Column O
+    print("EVPs: " + EVPs + "\n")
+    #show stats leaders for the tourney?
+
+
+
